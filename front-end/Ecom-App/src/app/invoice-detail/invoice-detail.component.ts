d7a6d820334b09ef3312159a1419bb50d8492baf
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2020-06-08 22:21:56 +0700

    Update invoice-detail component

--- front-end/Ecom-App/src/app/invoice-detail/invoice-detail.component.ts
+++ front-end/Ecom-App/src/app/invoice-detail/invoice-detail.component.ts
@@ -10,10 +10,17 @@ export class InvoiceDetailComponent implements OnInit {
   // Info about invoice detail
   invoiceId:string = '135178';
   receiver:string = 'Anny Farisha';
+  receiverPhone:string = '037 767 0509';
   emailOrderer:string = 'Anny.123@hotmail.com';
   invoiceAddress:string = '1642 Cambridge Drive, Phoenix, 85029 Arizona Arizona Phoenix';
   invoiceDate:string = 'Saturday, April 07 2019';
   dueDate:string = 'Sunday, April 19 2019';
+
+  invoiceStatus:string = "Pending";
+  invoiceStatusChange:string = "Pending";
+  isSave:boolean = false;
+
+  invoicePrice:number = 150;
   lstProduct = [
     {productId: 1, productTitle: 'Green Crack', Qty: 1, Price: 30},
     {productId: 1, productTitle: 'Green Crack', Qty: 1, Price: 30},
@@ -32,6 +39,19 @@ export class InvoiceDetailComponent implements OnInit {
     for (let product of this.lstProduct){
       this.invoiceTotal += product.Price * product.Qty;
     }
+  }
+
+  ChangeInvoiceStatus(){
+    if(this.invoiceStatusChange == "Paid"){
+      this.invoiceStatusChange = "Pending";
+    }else if(this.invoiceStatusChange == "Pending"){
+      this.invoiceStatusChange = "Transporting";
+    }else if (this.invoiceStatusChange == "Transporting"){
+      this.invoiceStatusChange = "Paid";
+    }
+
+    this.isSave =  this.invoiceStatusChange  != this.invoiceStatus;
+    
   }
 
 }
